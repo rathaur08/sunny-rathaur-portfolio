acfcc1007b5bc3e089e42fe0c229d59318fd51eb
Sunny <sunnyrathaur2444@gmail.com>
2024-12-12 22:41:29 +0530

    add Home page Tate

--- src/app/page.js
+++ src/app/page.js
@@ -6,12 +6,14 @@ export default function Home() {
     <>
       <Header />
       <main className="container">
-        <div className="pt-3">
+        <div className="pt-5">
           <div className="row">
             <div className="col-lg-6 col-md-6 col-sm-12 home-banner-text">
               <div className="">
-                <h1 className="">JetSetGO</h1>
-                <p className="">Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Aenean commodo ligula eget dolor. Aenean massa. Cum sociis natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Donec quam felis, ultricies nec, pellentesque eu, pretium quis, sem. Nulla consequat massa quis enim. Donec pede justo, fringilla vel, aliquet nec, vulputate eget, arcu. In enim justo, rhoncus ut, imperdiet a, venenatis vitae, justo. Nullam dictum felis eu pede mollis pretium. Integer tincidunt. Cras dapibus. Vivamus elementum semper nisi. Aenean vulputate eleifend tellus. Aenean leo ligula, porttitor eu, consequat vitae, eleifend ac, enim. Aliquam lorem ante, dapibus in, viverra quis, feugiat a, tellus. Phasellus viverra nulla ut metus varius laoreet. Quisque rutrum. Aenean imperdiet. Etiam ultricies nisi vel augue. Curabitur ullamcorper ultricies nisi. Nam eget dui. Etiam rhoncus. Maecenas tempus, tellus eget condimentum</p>
+                <h1 className="">Sunny Rathaur</h1>
+                <p className="">Hi, I’m Sunny Rathaur, a passionate Front-End Developer with 2 years of experience crafting user-friendly, responsive, and visually appealing web applications. I specialize in modern front-end technologies, including HTML, CSS, JavaScript, React.js, and Next.js, to deliver seamless user experiences.</p>
+                <p className="">As a creative problem-solver, I thrive on building intuitive interfaces and bringing design ideas to life. Whether collaborating with teams or working independently, I focus on clean code, attention to detail, and staying up-to-date with the latest industry trends.</p>
+                <p className="">Explore my portfolio to see how I turn ideas into reality. Let’s connect and create something amazing together!</p>
                 <button className="btn btn-primary">Let her Started </button>
               </div>
             </div>

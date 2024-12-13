670627e1bea14c73a13fc931378d7806b35d5656
Sunny Rathaur <sunny.rathaur@jetsetgo.in>
2024-12-13 16:00:41 +0530

    Update Header OR Footer AND Other Some Fix

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,8 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
 
+const nextConfig = {
+  images: {
+    domains: ["d3tfanr7troppj.cloudfront.net", "cdn-icons-png.flaticon.com"],
+  }
+};
 export default nextConfig;
--- src/app/_components/Footer.js
+++ src/app/_components/Footer.js
@@ -1,39 +1,41 @@
 import Image from "next/image";
+import Link from "next/link";
 
 const Footer = () => {
   return (
     <div className="container">
       <footer className="d-flex flex-wrap justify-content-between align-items-center py-3 my-4 border-top">
         <div className="col-md-4 d-flex align-items-center">
-          <a href="/" className="mb-3 me-2 mb-md-0 text-muted text-decoration-none lh-1">
-            <Image src="https://d3tfanr7troppj.cloudfront.net/static_files/images/000/006/330/original/Fab_icon.svg?1733917004" width={30} height={30} alt="SUNNY LOGO" />
-          </a>
-          <span className="mb-3 mb-md-0 text-muted">&copy; 2025 SR</span>
+          <Link href="/" className="mb-3 me-2 mb-md-0 text-muted text-decoration-none lh-1">
+            <Image src="https://d3tfanr7troppj.cloudfront.net/static_files/images/000/006/330/original/Fab_icon.svg?1733917004" width={40} height={40} alt="SUNNY LOGO" />
+          </Link>
+          <span className="mb-3 mb-md-0 text-muted">&copy; 2025 All right reserved by SR.</span>
         </div>
         <ul className="nav col-md-4 justify-content-end list-unstyled d-flex">
           <li className="ms-3">
-            <a className="text-muted" href="#">
-              twitter
-              <svg className="bi" width="24" height="24">
-                <use xlinkHref="#twitter" />
-              </svg>
-            </a>
+            <Link className="text-muted" href="https://github.com/rathaur08">
+              <Image className="icon-primary-color" src="https://cdn-icons-png.flaticon.com/128/14715/14715341.png" width={25} height={25} alt="icon" />
+            </Link>
           </li>
           <li className="ms-3">
-            <a className="text-muted" href="#">
-              instagram
-              <svg className="bi" width="24" height="24">
-                <use xlinkHref="#instagram" />
-              </svg>
-            </a>
+            <Link className="text-muted" href="https://github.com/rathaur08">
+              <Image className="icon-primary-color" src="https://cdn-icons-png.flaticon.com/128/254/254412.png" width={25} height={25} alt="icon" />
+            </Link>
           </li>
           <li className="ms-3">
-            <a className="text-muted" href="#">
-              facebook
-              <svg className="bi" width="24" height="24">
-                <use xlinkHref="#facebook" />
-              </svg>
-            </a>
+            <Link className="text-muted" href="https://github.com/rathaur08">
+              <Image className="icon-primary-color" src="https://cdn-icons-png.flaticon.com/128/5968/5968830.png" width={25} height={25} alt="icon" />
+            </Link>
+          </li>
+          <li className="ms-3">
+            <Link className="text-muted" href="https://github.com/rathaur08">
+              <Image className="icon-primary-color" src="https://cdn-icons-png.flaticon.com/128/254/254390.png" width={25} height={25} alt="icon" />
+            </Link>
+          </li>
+          <li className="ms-3">
+            <Link className="text-muted" href="https://github.com/rathaur08">
+              <Image className="icon-primary-color" src="https://cdn-icons-png.flaticon.com/128/3291/3291695.png" width={25} height={25} alt="icon" />
+            </Link>
           </li>
         </ul>
       </footer>

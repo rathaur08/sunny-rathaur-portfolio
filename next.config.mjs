d7259c9e01a333a05c8bc3e5b21171cca9970d1f
Sunny <sunnyrathaur2444@gmail.com>
2024-12-16 22:37:10 +0530

    build update code

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,9 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  output: "export",
+  // Specify the base path for GitHub Pages
+  basePath: "/sunny-rathaur-portfolio",
+  assetPrefix: "/sunny-rathaur-portfolio",
+};
 
 export default nextConfig;
